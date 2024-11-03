a7a6f62e3f36ebb59d425a3cdea55698b570a29f
Enriquegonzalezz <enriquealfonzo123@gmail.com>
2024-11-03 11:59:04 -0400

    fix it login

--- vistas/login.js
+++ vistas/login.js
@@ -2,6 +2,10 @@ const cambio = document.getElementById('cambio');
 const login = document.querySelector('.login');
 const signin = document.querySelector('.signin');
 const regreso = document.getElementById('regreso');
+const forgot = document.getElementById('forgot');
+const password = document.querySelector('.forg');
+const flechi = document.querySelector('.flechi');
+
 signin.style.display = 'none';
 
 cambio.addEventListener('click', ()=>{
@@ -12,4 +16,17 @@ cambio.addEventListener('click', ()=>{
 regreso.addEventListener('click', ()=>{
     signin.style.display = 'none';
     login.style.display = 'flex';
+})
+
+forgot.addEventListener('click', ()=>{
+    signin.style.display = 'none';
+    login.style.display = 'none';
+    password.style.display= 'flex';
+
+});
+
+flechi.addEventListener('click', ()=> {
+    password.style.display = 'none';
+    login.style.display = 'flex';
+    signin.style.display = 'none';
 })
